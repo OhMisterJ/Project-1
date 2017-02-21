ed0eb46f1701aeaaf6789534bc3bd2ff38a0883f
Student <student@ECAW01150-PC07.local>
2017-02-21 14:04:56 -0500

    Project 1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
+        <title>Project 1 - Quiz</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -22,10 +22,43 @@
         <![endif]-->
 
         <!-- Add your site or application content here -->
+        <h1>Your Results!</h1>
         <?php 
-        /* Write your PHP here */
+            $answer1 = $_POST['question-1-answers'];
+            $answer2 = $_POST['question-2-answers'];
+            $answer3 = $_POST['question-3-answers'];
+            $answer4 = $_POST['question-4-answers'];
+            $answer5 = $_POST['question-5-answers'];
+            $answer6 = $_POST['question-6-answers'];
+            $answer7 = $_POST['question-7-answers'];
+            $answer8 = $_POST['question-8-answers'];
+            $answer9 = $_POST['question-9-answers'];
+            $answer10 = $_POST['question-10-answers'];
         
+            $totalCorrect = 0;
+            
+            if ($answer1 == "A") { $totalCorrect++; }
+            if ($answer2 == "C") { $totalCorrect++; }
+            if ($answer3 == "B") { $totalCorrect++; }
+            if ($answer4 == "A") { $totalCorrect++; }
+            if ($answer5 == "D") { $totalCorrect++; }
+            if ($answer6 == "A") { $totalCorrect++; }
+            if ($answer7 == "B") { $totalCorrect++; }
+            if ($answer8 == "B") { $totalCorrect++; }
+            if ($answer9 == "A") { $totalCorrect++; }
+            if ($answer10 == "D") { $totalCorrect++; }
+
+                if ($totalCorrect == 10) {
+                echo '<br>Perfect! Congratulations!';
+            }   else if ($totalCorrect >= 6,7,8,9) {
+                echo '<br>You passed!';
+            }   else if ($totalCorrect <= 5) {
+                echo '<br>You failed. Please try again!';
+            }
+            
+            echo "<div id='results'>$totalCorrect / 10 correct</div>";
         ?> 
+
 
            
 
